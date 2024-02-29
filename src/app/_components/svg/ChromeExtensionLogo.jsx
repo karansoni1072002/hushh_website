57cb67e72db9f28465068179ba34879233b4f6e5
Karan <karansoni1072002@gmail.com>
2024-03-01 03:29:16 +0530

    Homepage

---
+++ src/app/_components/svg/ChromeExtensionLogo.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ChromeExtentionLogo = () => <></>;
+
+export default ChromeExtentionLogo;
---
+++ src/app/layout.jsx
@@ -0,0 +1,31 @@
+import './globals.css'
+import { Navbar } from './_components/Navbar'
+import { Footer } from './_components/Footer'
+import { mulish } from './_utilities/fonts'
+import HomebannerBG from './_components/svg/images/homeBannerBg'
+import { Providers } from './provider'
+import Header from './_components/header'
+import React from 'react'
+export const metadata = {
+  title: 'HUSHH',
+  description: 'Your data Your Business',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+
+      <body className={`${mulish.variable} font-sans`} >
+        <header className='h-full absolute z-50'>
+          <Header />
+        </header>
+          <Providers>
+              {children}
+           </Providers>
+        {/* <footer>
+          <Footer />
+        </footer> */}
+      </body>
+    </html>
+  );
+}

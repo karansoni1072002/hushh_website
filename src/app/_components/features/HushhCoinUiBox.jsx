c12e464795edb35b85a02b3f12d9cb49a39670ba
Neelesh <damrianeelesh@gmail.com>
2024-03-07 19:06:43 +0530

    more changes added

--- src/app/_components/features/HushhCoinUiBox.jsx
+++ src/app/_components/features/HushhCoinUiBox.jsx
@@ -1,31 +1,41 @@
-import { Container, Box, VStack, Heading, Text} from '@chakra-ui/react';
+import { Container, Box, VStack, Heading, Text } from '@chakra-ui/react';
 import NextImage from "next/image";
-import React from 'react'
+import React from 'react';
 import LVCardMobile from "../../../../public/Images/mobileIcons/LVCardMobile.png";
+import MarketIcon from '../svg/marketIcon';
+import extendedTheme from '../../theme';
 
 const HushhCoinUiBox = () => {
   return (
-    <>
-       <Container w={'100%'} mt={'5rem'} display={'flex'} flexDirection={'row'}>
-          <Box>
-            <NextImage
-              src={LVCardMobile}
-              alt='CoinBox'
-              />
+    <Container mt={{ base: '2rem', md: '5rem' }} display="flex" alignItems="center" justifyContent="space-between" flexDirection={{ base: 'column', md: 'row' }}>
+      {/* <Box maxWidth={{ base: '100%', md: '40%' }}>
+        <NextImage src={LVCardMobile} alt="CoinBox" />
+      </Box> 
+      */}
+      <VStack alignItems={{ base: 'center', md: 'flex-start' }} maxW={{ base: '100%', md: '50%' }} textAlign={{ base: 'center', md: 'left' }}>
+        <Heading className="gradient" fontWeight="700" fontSize={{ md: '3.75rem', base: '2rem' }}>
+          Earn hushh coins on your valuable data sharing
+        </Heading>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nulla suspendisse tortor aene.
+        </Text>
+        <VStack align="flex-start" spacing={4} w="100%">
+          <Box display="flex" alignItems="center">
+            <MarketIcon />
+            <Text color={extendedTheme.colors._white}>Lowest fees in market</Text>
           </Box>
-          <Box>
-              <VStack>
-                <Heading className='gradient'>
-                Earn hushh coins on your valuable data sharing
-                </Heading>
-                <Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nulla suspendisse tortor aene.
-                </Text>
-              </VStack>
+          <Box display="flex" alignItems="center">
+            <MarketIcon />
+            <Text color={extendedTheme.colors._white}>Lowest fees in market</Text>
           </Box>
-       </Container>
-    </>
-  )
-}
+          <Box display="flex" alignItems="center">
+            <MarketIcon />
+            <Text color={extendedTheme.colors._white}>Lowest fees in market</Text>
+          </Box>
+        </VStack>
+      </VStack>
+    </Container>
+  );
+};
 
 export default HushhCoinUiBox;

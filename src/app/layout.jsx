73c7533b1bb7407f39e437e0a38e3588eebdd8ba
Karan <karansoni1072002@gmail.com>
2024-03-14 02:24:08 +0530

    Footer Completed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,12 @@
 import './globals.css'
 import { Navbar } from './_components/Navbar'
-import { Footer } from './_components/Footer'
 import { mulish } from './_utilities/fonts'
 import HomebannerBG from './_components/svg/images/homeBannerBg'
 import { Providers } from './provider'
 import Header from './_components/header'
 import React from 'react'
 import ResponsiveSizeProvider from './context/responsive'
-
+import ContactForm from './_components/features/contactForm'
 export const metadata = {
   title: 'HUSHH',
   description: 'Your data Your Business',
@@ -17,17 +16,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-      <body className={`${mulish.variable} font-sans`} >
+      <body className={`${mulish.variable} font-sans relative`} >
         <ResponsiveSizeProvider>
-        <header className='h-full absolute z-50'>
-          <Header />
-        </header>
-          <Providers>
+          <header className='h-full absolute z-50'>
+            <Header />
+          </header>
+          <div className="">
+            <Providers>
               {children}
-           </Providers>
-        {/* <footer>
-          <Footer />
-        </footer> */}
+            </Providers>
+          </div>
         </ResponsiveSizeProvider>
       </body>
     </html>

c6587d570e843dd77b5e57807028029e490e33b5
Neelesh <damrianeelesh@gmail.com>
2024-03-26 19:15:40 +0530

    adding all advisors Data in an array of objects

---
+++ src/app/_components/advisorsBioData/advisorsData.jsx
@@ -0,0 +1,93 @@
+export const advisorsData = [
+    {
+      name: "Suresh Attuluri",
+      position: "Technology Advisor - Engineering",
+      linkedin: "https://www.linkedin.com/in/sureshattuluri/",
+      imageSrc: teamImages.ApoorvBedmutha,
+    },
+    {
+      name: "Mark McLaughlin",
+      position: "Advisor to the CEO, Chairman Of The Board at Qualcomm",
+      linkedin: "https://www.linkedin.com/in/markmclaughlin4/",
+      imageSrc: teamImages.SubashManoharan,
+    },
+    {
+      name: "Mark Groves",
+      position: "Technology Advisor - Products",
+      linkedin: "https://www.linkedin.com/in/mark-groves/",
+      imageSrc: teamImages.RohanSidankar,
+    },
+    {
+      name: "Dan Sareen",
+      position: "GTM Advisor - Ecosytems and Partnerships",
+      linkedin: "https://www.linkedin.com/in/dan-sareen-95259a274/",
+      imageSrc: teamImages.SatyamArora,
+    },
+    {
+      name: "Andrew Stein",
+      position: "Technology Advisor - Products",
+      linkedin: "https://www.linkedin.com/in/ahstein/",
+      imageSrc: teamImages.OmkarKailasMalpure,
+    },
+    {
+      name: "Shirley Pellicer",
+      position: "GTM Business Developer - Luxury Markets",
+      linkedin: "https://www.linkedin.com/in/shirleypellicer/",
+      imageSrc: teamImages.KavyaChauhan,
+    },
+    {
+      name: "Stéphane Spinella",
+      position: "GTM Advisor - EMEA Sales and Business Development",
+      linkedin: "https://www.linkedin.com/in/stephanespinella/",
+      imageSrc: teamImages.DeveshParagiri,
+    },
+    {
+      name: "Amit Fulay",
+      position: "Technology Advisor - Products",
+      linkedin: "https://www.linkedin.com/in/afulay/",
+      imageSrc: teamImages.AnkitKumarSingh,
+    },
+    {
+      name: "Rish Tandon",
+      position: "Technology Advisor - Products",
+      linkedin: "https://www.linkedin.com/in/rishtandon/",
+      imageSrc: teamImages.YashMakan,
+    },
+    {
+      name: "Valliappa Lakshmanan",
+      position: "Technology Advisor - AI/ML, Data and Analytics",
+      linkedin: "https://www.linkedin.com/in/valliappalakshmanan/",
+      imageSrc: teamImages.AnuragGhose,
+    },
+    {
+      name: "Sid Gavandi",
+      position: "User Experience Advisor - Products",
+      linkedin: "https://www.linkedin.com/in/sid-gavandi/",
+      imageSrc: teamImages.AkashKatla,
+    },
+    {
+      name: "Sudhir Hasbe",
+      position: "Technology Advisor - Products",
+      linkedin: "https://www.linkedin.com/in/shasbe/",
+      imageSrc: teamImages.KaranSoni,
+    },
+    {
+      name: "Samant Nagpal",
+      position: "Business Advisor - Corporate Risk",
+      linkedin: "https://www.linkedin.com/in/samant-nagpal-3240683/",
+      imageSrc: teamImages.NeeleshMeena,
+    },
+    {
+      name: "Daniel Cleveland",
+      position: "Business Advisor - Finance",
+      linkedin: "https://www.linkedin.com/in/danielcleveland/",
+      imageSrc: teamImages.ApoorvBedmutha,
+    },
+    {
+      name: "Nilanshu Raja",
+      position: "Business Advisor  - Finance",
+      linkedin: "https://www.linkedin.com/in/nilanshu/",
+      imageSrc: teamImages.KarthickMuthu,
+    },
+  ];
+  

d0f282876780993501d83878a9650d2fc73ac64d
Neelesh <damrianeelesh@gmail.com>
2024-04-07 18:24:52 +0530

    text fixes

--- src/app/legal/termsofuse/page.jsx
+++ src/app/legal/termsofuse/page.jsx
@@ -37,7 +37,7 @@ const TermsOfUse = () => {
                         <br />
                         Ownership of Site; Agreement to Terms of Use
                         <br />
-                        These Terms and Conditions of Use (the "Terms of Use") apply to the Hushone, Inc. web site located at www.hushh.ai, and all associated sites linked to www.hushh.ai by Hushone, Inc., its subsidiaries and affiliates, including Hushone sites around the world (collectively, the "Site"). The Site is the property of Hushone, Inc. ("Hushone") and its licensors. BY USING THE SITE, YOU AGREE TO THESE TERMS OF USE; IF YOU DO NOT AGREE, DO NOT USE THE SITE.
+                        These Terms and Conditions of Use (the "Terms of Use") apply to Hushone, Inc. web site located at www.hushh.ai, and all associated sites linked to www.hushh.ai by Hushone, Inc., its subsidiaries and affiliates, including Hushone sites around the world (collectively, the "Site"). The Site is the property of Hushone, Inc. ("Hushone") and its licensors. BY USING THE SITE, YOU AGREE TO THESE TERMS OF USE; IF YOU DO NOT AGREE, DO NOT USE THE SITE.
                         <br />
                         Hushone reserves the right, at its sole discretion, to change, modify, add or remove portions of these Terms of Use, at any time. It is your responsibility to check these Terms of Use periodically for changes. Your continued use of the Site following the posting of changes will mean that you accept and agree to the changes. As long as you comply with these Terms of Use, Hushone grants you a personal, non-exclusive, non-transferable, limited privilege to enter and use the Site.
                         <br />

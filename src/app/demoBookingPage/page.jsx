13c16e04255a0d3f6137100a83890968e7ab11da
Neelesh <damrianeelesh@gmail.com>
2024-04-08 17:52:25 +0530

    form addition

--- src/app/demoBookingPage/page.jsx
+++ src/app/demoBookingPage/page.jsx
@@ -9,16 +9,104 @@ import {
   List,
   ListIcon,
   ListItem,
+  Select,
   Stack,
   Text,
   VStack,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import BlueTick from "../_components/svg/icons/blueTick.svg";
-import HushhButtonDemo from "../_components/svg/hushhButtonDemo.svg"
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  Button,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import HushhButtonDemo from "../_components/svg/hushhButtonDemo.svg";
 
 const DemoBookingPage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    product: "",
+  });
+
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    product: "",
+  });
+
+  const products = [
+    "Hushh Button",
+    "Hushh Wallet App",
+    "Browser Comapnion",
+    "Vibe Search",
+    "All",
+  ];
+
+  const validateForm = () => {
+    let valid = true;
+    const newErrors = { ...errors };
+
+    if (!formData.name) {
+      newErrors.name = "Name is required";
+      valid = false;
+    } else {
+      newErrors.name = "";
+    }
+
+    if (!formData.email) {
+      newErrors.email = "Email is required";
+      valid = false;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Invalid email format";
+      valid = false;
+    } else {
+      newErrors.email = "";
+    }
+
+    if (!formData.phoneNumber) {
+      newErrors.phoneNumber = "Phone number is required";
+      valid = false;
+    } else if (!/^\d{10}$/.test(formData.phoneNumber)) {
+      newErrors.phoneNumber = "Invalid phone number";
+      valid = false;
+    } else {
+      newErrors.phoneNumber = "";
+    }
+
+    if (!formData.product) {
+      newErrors.product = "Product selection is required";
+      valid = false;
+    } else {
+      newErrors.product = "";
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      // Redirect to the Calendly page with form data as query parameters
+      window.location.href = `/schedule-call?name=${formData.name}&email=${formData.email}&message=${formData.message}`;
+    }
+  };
   return (
     <>
       <Box p={{ base: 4, md: 8 }}>
@@ -78,12 +166,74 @@ const DemoBookingPage = () => {
               if it aligns with your needs.
             </Text>
           </Box>
-          <Box
-            borderRadius={"1rem"}
-            bg={"#151515"}
-            display={"flex"}
-            flex={1}
-          ></Box>
+          <Box borderRadius={"1rem"} bg={"#151515"} display={"flex"} flex={1}>
+            <form
+              style={{ padding: "2rem", width: "100%" }}
+              onSubmit={handleSubmit}
+            >
+              <FormControl isInvalid={errors.name}>
+                <FormLabel color={"#FFFFFF"}>Name*</FormLabel>
+                <Input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  placeholder="Name"
+                />
+                <FormErrorMessage>{errors.name}</FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={errors.email} mt={4}>
+                <FormLabel color={"#FFFFFF"}>Business Email*</FormLabel>
+                <Input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Email"
+                />
+                <FormErrorMessage>{errors.email}</FormErrorMessage>
+              </FormControl>
+
+              <FormControl isInvalid={errors.product} mt={4}>
+                <FormLabel color={"#FFFFFF"}>Product</FormLabel>
+                <Select
+                  name="product"
+                  value={formData.product}
+                  onChange={handleChange}
+                  placeholder="Select Product"
+                >
+                  {products.map((product, index) => (
+                    <option key={index} value={product}>
+                      {product}
+                    </option>
+                  ))}
+                </Select>
+                <FormErrorMessage>{errors.product}</FormErrorMessage>
+              </FormControl>
+
+              <FormControl isInvalid={errors.phoneNumber} mt={4}>
+                <FormLabel
+                  color={"#FFFFFF"}
+                  fontWeight={"400"}
+                  fontSize={{ md: "14px" }}
+                >
+                  Phone Number
+                </FormLabel>
+                <Input
+                  type="tel"
+                  name="phoneNumber"
+                  value={formData.phoneNumber}
+                  onChange={handleChange}
+                  placeholder="Phone Number"
+                />
+                <FormErrorMessage>{errors.phoneNumber}</FormErrorMessage>
+              </FormControl>
+
+              <Button type="submit" mt={4} colorScheme="teal">
+                Submit
+              </Button>
+            </form>
+          </Box>
         </Stack>
 
         <VStack
@@ -110,25 +260,125 @@ const DemoBookingPage = () => {
             Interactive demo videos
           </Heading>
           <Grid templateColumns="repeat(4, 1fr)" gap={6}>
-            <GridItem borderRadius={'8px'} p={'1rem'} display={'flex'} flexDirection={'column'} border={'1px solid #4B4B4B'} bg={'#121212'}>
-               <Image src={HushhButtonDemo} alt="HushhButtonDemo"/>
-               <Text color={'#FFFFFF'} fontWeight={'400'} lineHeight={'32px'} fontSize={{md:'1.5rem', base:'1rem'}} letterSpacing={'-0.2px'} mt={{md:'1.25rem', base:'0.75rem'}}>Hushh Button</Text>
-               <Text color={'#0565FF'} fontWeight={'400'} letterSpacing={'-0.4px'} lineHeight={'26px'} fontSize={{md:'1rem', base:'0.65rem'}} my={{md:'2.15rem', base:'1.15rem'}}>Watch Now</Text>
+            <GridItem
+              borderRadius={"8px"}
+              p={"1rem"}
+              display={"flex"}
+              flexDirection={"column"}
+              border={"1px solid #4B4B4B"}
+              bg={"#121212"}
+            >
+              <Image src={HushhButtonDemo} alt="HushhButtonDemo" />
+              <Text
+                color={"#FFFFFF"}
+                fontWeight={"400"}
+                lineHeight={"32px"}
+                fontSize={{ md: "1.5rem", base: "1rem" }}
+                letterSpacing={"-0.2px"}
+                mt={{ md: "1.25rem", base: "0.75rem" }}
+              >
+                Hushh Button
+              </Text>
+              <Text
+                color={"#0565FF"}
+                fontWeight={"400"}
+                letterSpacing={"-0.4px"}
+                lineHeight={"26px"}
+                fontSize={{ md: "1rem", base: "0.65rem" }}
+                my={{ md: "2.15rem", base: "1.15rem" }}
+              >
+                Watch Now
+              </Text>
             </GridItem>
-            <GridItem borderRadius={'8px'} p={'1rem'} display={'flex'} flexDirection={'column'} border={'1px solid #4B4B4B'} bg={'#121212'}>
-               <Image src={HushhButtonDemo} alt="HushhButtonDemo"/>
-               <Text color={'#FFFFFF'} fontWeight={'400'} lineHeight={'32px'} fontSize={{md:'1.5rem', base:'1rem'}} letterSpacing={'-0.2px'} mt={{md:'1.25rem', base:'0.75rem'}}>Hushh Button</Text>
-               <Text color={'#0565FF'} fontWeight={'400'} letterSpacing={'-0.4px'} lineHeight={'26px'} fontSize={{md:'1rem', base:'0.65rem'}} my={{md:'2.15rem', base:'1.15rem'}}>Watch Now</Text>
+            <GridItem
+              borderRadius={"8px"}
+              p={"1rem"}
+              display={"flex"}
+              flexDirection={"column"}
+              border={"1px solid #4B4B4B"}
+              bg={"#121212"}
+            >
+              <Image src={HushhButtonDemo} alt="HushhButtonDemo" />
+              <Text
+                color={"#FFFFFF"}
+                fontWeight={"400"}
+                lineHeight={"32px"}
+                fontSize={{ md: "1.5rem", base: "1rem" }}
+                letterSpacing={"-0.2px"}
+                mt={{ md: "1.25rem", base: "0.75rem" }}
+              >
+                Browser Companion
+              </Text>
+              <Text
+                color={"#0565FF"}
+                fontWeight={"400"}
+                letterSpacing={"-0.4px"}
+                lineHeight={"26px"}
+                fontSize={{ md: "1rem", base: "0.65rem" }}
+                my={{ md: "2.15rem", base: "1.15rem" }}
+              >
+                Watch Now
+              </Text>
             </GridItem>
-            <GridItem borderRadius={'8px'} p={'1rem'} display={'flex'} flexDirection={'column'} border={'1px solid #4B4B4B'} bg={'#121212'}>
-               <Image src={HushhButtonDemo} alt="HushhButtonDemo"/>
-               <Text color={'#FFFFFF'} fontWeight={'400'} lineHeight={'32px'} fontSize={{md:'1.5rem', base:'1rem'}} letterSpacing={'-0.2px'} mt={{md:'1.25rem', base:'0.75rem'}}>Hushh Button</Text>
-               <Text color={'#0565FF'} fontWeight={'400'} letterSpacing={'-0.4px'} lineHeight={'26px'} fontSize={{md:'1rem', base:'0.65rem'}} my={{md:'2.15rem', base:'1.15rem'}}>Watch Now</Text>
+            <GridItem
+              borderRadius={"8px"}
+              p={"1rem"}
+              display={"flex"}
+              flexDirection={"column"}
+              border={"1px solid #4B4B4B"}
+              bg={"#121212"}
+            >
+              <Image src={HushhButtonDemo} alt="HushhButtonDemo" />
+              <Text
+                color={"#FFFFFF"}
+                fontWeight={"400"}
+                lineHeight={"32px"}
+                fontSize={{ md: "1.5rem", base: "1rem" }}
+                letterSpacing={"-0.2px"}
+                mt={{ md: "1.25rem", base: "0.75rem" }}
+              >
+                Vibe Search
+              </Text>
+              <Text
+                color={"#0565FF"}
+                fontWeight={"400"}
+                letterSpacing={"-0.4px"}
+                lineHeight={"26px"}
+                fontSize={{ md: "1rem", base: "0.65rem" }}
+                my={{ md: "2.15rem", base: "1.15rem" }}
+              >
+                Watch Now
+              </Text>
             </GridItem>
-            <GridItem borderRadius={'8px'} p={'1rem'} display={'flex'} flexDirection={'column'} border={'1px solid #4B4B4B'} bg={'#121212'}>
-               <Image src={HushhButtonDemo} alt="HushhButtonDemo"/>
-               <Text color={'#FFFFFF'} fontWeight={'400'} lineHeight={'32px'} fontSize={{md:'1.5rem', base:'1rem'}} letterSpacing={'-0.2px'} mt={{md:'1.25rem', base:'0.75rem'}}>Hushh Button</Text>
-               <Text color={'#0565FF'} fontWeight={'400'} letterSpacing={'-0.4px'} lineHeight={'26px'} fontSize={{md:'1rem', base:'0.65rem'}} my={{md:'2.15rem', base:'1.15rem'}}>Watch Now</Text>
+            <GridItem
+              borderRadius={"8px"}
+              p={"1rem"}
+              display={"flex"}
+              flexDirection={"column"}
+              border={"1px solid #4B4B4B"}
+              bg={"#121212"}
+            >
+              <Image src={HushhButtonDemo} alt="HushhButtonDemo" />
+              <Text
+                color={"#FFFFFF"}
+                fontWeight={"400"}
+                lineHeight={"32px"}
+                fontSize={{ md: "1.5rem", base: "1rem" }}
+                letterSpacing={"-0.2px"}
+                mt={{ md: "1.25rem", base: "0.75rem" }}
+              >
+                Hushh App
+              </Text>
+              <Text
+                color={"#0565FF"}
+                fontWeight={"400"}
+                letterSpacing={"-0.4px"}
+                lineHeight={"26px"}
+                fontSize={{ md: "1rem", base: "0.65rem" }}
+                my={{ md: "2.15rem", base: "1.15rem" }}
+              >
+                Watch Now
+              </Text>
             </GridItem>
           </Grid>
         </VStack>

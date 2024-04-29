371f10a59d96ee975ebc628f05e6d0d3fd244179
Neelesh <damrianeelesh@gmail.com>
2024-04-29 12:08:19 +0530

    adding hushh blog page

---
+++ src/app/hushhBlogs/page.jsx
@@ -0,0 +1,17 @@
+import { allBlogs } from "../../../.contentlayer/generated";
+import HomeCoverSection from "../_components/blogs/HomeCoverSection";
+import FeaturedPosts from "../_components/blogs/FeaturedPosts";
+import RecentPosts from "../_components/blogs/RecentPosts";
+
+export default function hushhBlogs() {
+  
+  return (
+    <main className="flex flex-col items-center justify-center">
+      <HomeCoverSection blogs={allBlogs} />
+      <FeaturedPosts blogs={allBlogs} />
+      <RecentPosts blogs={allBlogs} />
+
+
+    </main>
+  )
+}

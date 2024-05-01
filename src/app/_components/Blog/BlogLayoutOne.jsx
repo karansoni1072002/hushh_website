4b5f4f8a5dc938d2d82c237d1db79daec40f8e48
Neelesh <damrianeelesh@gmail.com>
2024-05-01 10:40:22 +0530

    tag fixes

--- src/app/_components/Blog/BlogLayoutOne.jsx
+++ src/app/_components/Blog/BlogLayoutOne.jsx
@@ -25,7 +25,7 @@ const BlogLayoutOne = ({ blog }) => {
         className="px-6 text-xs  sm:text-sm py-1 sm:py-2 !border "
         />
         <Link href={blog.url} className="mt-6">
-          <h2 className="font-bold capitalize text-sm xs:text-base sm:text-xl md:text-2xl text-white mt-2 sm:mt-4">
+          <h2 className="font-bold blogLayoutOneTitle capitalize text-sm xs:text-base sm:text-xl md:text-2xl text-white mt-2 sm:mt-4">
             <span
               className="bg-gradient-to-r from-accent to-accent bg-[length:0px_6px] dark:from-accentDark/50 dark:to-accentDark/50
                 group-hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 "

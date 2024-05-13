827aa1608868395d13be83ee2bb44ec063a3b7b7
Neelesh <damrianeelesh@gmail.com>
2024-05-13 15:25:27 +0530

    removing supabase credentials

--- src/app/products/developerApi/page.jsx
+++ src/app/products/developerApi/page.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { cookies } from "next/headers";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { cookies } from "next/headers";
+// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default  function DeveloperApi(){
 
-    const cookieStore = cookies();
-    const supabase = createServerComponentClient({cookies: () => cookieStore});
-    const {data: {user}} =  supabase.auth.getUser()
+    // const cookieStore = cookies();
+    // const supabase = createServerComponentClient({cookies: () => cookieStore});
+    // const {data: {user}} =  supabase.auth.getUser()
 
   return (
     <div>Whole UI for developer page </div>

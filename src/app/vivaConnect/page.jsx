ef1103faab6ebacda8137fb4b29846f989210da2
Neelesh <damrianeelesh@gmail.com>
2024-05-20 20:01:15 +0530

    adding new viva connect

--- src/app/vivaConnect/page.jsx
+++ src/app/vivaConnect/page.jsx
@@ -9,7 +9,8 @@ import {
   Icon,
   Flex,
   useToast,
-  useBreakpointValue
+  useBreakpointValue,
+  Stack,
 } from "@chakra-ui/react";
 import {
   FaCalendarAlt,
@@ -36,6 +37,15 @@ import { ChromeExtension } from "../_components/svg/icons/HeaderIcons/ChromeExte
 import VibeSearchApi from "../_components/svg/vibeSearchApi";
 import { VibeSearchMarketplace } from "../_components/svg/icons/HeaderIcons/VibeSearchMarketplace";
 
+// New Icons for Mobile Screens
+import ChromeIcon from "../_components/svg/productIcons/chromeIcon.svg";
+import DeveloperIcon from "../_components/svg/productIcons/developerIcon.svg";
+import VibeSearchNew from "../_components/svg/productIcons/vibeSearchIcon.svg";
+import ButtonIcon from "../_components/svg/productIcons/hushhButtonIcon.svg";
+import HushhAppIcon from "../_components/svg/productIcons/hushhAppIcon.svg";
+import hushhTeam from "../_components/svg/productIcons/hushhTeamLogo.svg";
+import hushhLogo from "../_components/svg/productIcons/hushhLogo.svg";
+
 const buttonStyles = {
   baseStyle: {
     borderRadius: "8px",
@@ -57,254 +67,383 @@ export default function vivaConnect() {
   const router = useRouter();
   const toast = useToast();
   const iconSize = useBreakpointValue({ base: "xs", md: "sm" });
-  const buttonPadding = useBreakpointValue({ base: "0.2rem 0.8rem", md: "0.4rem 1.2rem" });
+  const buttonPadding = useBreakpointValue({
+    base: "0.2rem 0.8rem",
+    md: "0.4rem 1.2rem",
+  });
   const textSize = useBreakpointValue({ base: "0.8rem", md: "1rem" });
 
   return (
-    <Box
-      bg="black"
-      p={4}
-      color="white"
-      // position={"relative"}
-      mt={"2rem"}
-      // zIndex={"9999999999999999"}
-      fontFamily={"Poppins"}
-    >
-      <VStack
-        spacing={4}
-        px={"2rem"}
-        align="stretch"
-
-      >
-        <Flex justifyContent="space-between" alignItems="center" mb={4} w="100%">
-          <Text
-            color={"#FFFFFF"}
-            fontWeight={"400"}
-            fontSize={{ base:"1.5rem",md:'3rem'}}
-            letterSpacing={"-0.27px"}
-            lineHeight={"30px"}
-          >
-            Hushh
-          </Text>
+    <Box bg="black" p={4} color="white" mt={"1rem"} fontFamily={"Poppins"}>
+      <VStack fontFamily={"Poppins"} spacing={4} w="100%" maxW="400px">
+      <Flex
+          justifyContent="space-between"
+          alignItems="center"
+          // mb={2}
+          w="100%"
+        >
+          <Image
+            src={hushhLogo}
+            alt="hushhLogo"
+          />
           <Image
             style={{ cursor: "pointer" }}
             target="_blank"
             onClick={() =>
-              window.open(
-                "https://calendly.com/hushh/30min",
-                '_blank'
-              )
+              window.open("https://calendly.com/hushh/30min", "_blank")
             }
             src={CalendlyIcon}
             alt="calendlyIcon"
-            // width="30px"
-            // height="30px"
           />
         </Flex>
-        <Flex
-          direction="column" 
-          alignItems="center"
-          cursor={"pointer"}
+        <Box
+          bg={"transparent"}
+          borderRadius={"8px"}
           onClick={() => router.push("/qrCodePage")}
         >
-          <Image
-            src={TeamHushhThumb}
-            alt="Team Hushh"
-            boxSize="120px"
-            opacity={0.5}
-            filter="grayscale(100%)"
-          />
-          <Box mt={"-1rem"} zIndex={"9"} mb={"1rem"}>
-            <QRCode
-              size={"40"}
-              fgColor="#FFFFFF"
-              logoOpacity={"0.5"}
-              bgColor="transparent"
-              quietZone={"0"}
-              logoPaddingStyle="square"
-              logoPadding="0"
-              value="https://www.hush1one.com/vivaConnect"
-            />
-          </Box>
-          <Text
-            fontWeight={"400"}
-            fontSize={{ base:"1.15rem",md:'1.75rem',lg:"2rem"}}
-            lineHeight={"22.95px"}
-            color={"#FFFFFF"}
+          <Stack
+            gap={"0.5rem"}
+            display={"flex"}
+            flexDirection={"row"}
           >
-            Team Hushh 🤫
-          </Text>
-          <Text fontSize={{base:"12px",md:"18px",lg:"24px"}} color={"#A2A1A1"}>
-            Hushh: unlock the power of your data
-          </Text>
-        </Flex>
+            <Box
+              w={"100%"}
+              h={"100%"}
+              flex={0.3}
+              display={"flex"}
+              alignItems={"center"}
+              alignContent={"center"}
+              alignSelf={"center"}
+              justifyItems={"center"}
+              justifySelf={"center"}
+            >
+              <Image src={hushhTeam} alt="hushhTeam" objectFit="fill" />
+            </Box>
+            <VStack
+              gap={"0px"}
+              display={"flex"}
+              alignItems={"flex-start"}
+              textAlign={"left"}
+              flex={1}
+              p={2}
+            >
+              <Text color={"#FFFFFF"} fontWeight={"400"} lineHeight={'22.95px'} fontSize={"1.15rem"}>
+                Team Hushh
+              </Text>
+              <Text
+                fontWeight={"400"}
+                fontSize={"0.625rem"}
+                lineHeight={"12px"}
+                color={"#A2A1A1"}
+              >
+                Hushh helps you Collect, Organize, Manage, Share, Monetize, Value your data Take ownership. Build connections. Unlock your data's potential.
+              </Text>
+            </VStack>
+          </Stack>
+        </Box>
 
-        <Button
-          boxShadow={"4px 4px 0px 0px #BFBFBF"}
+        <Box
+          bg={"white"}
           borderRadius={"8px"}
           border={"1px solid #000000"}
-          display={"flex"}
-          w={'full'}
-          flexDirection={"row"}
-          justifyContent={"flex-start"}
-          bg={"white"}
-          color={"#484848"}
-          p={"0.1rem"}
+          boxShadow={"4px 4px 0px 0px #BFBFBF"}
           onClick={() =>
-            window.open(
-              "https://hushh-button.vercel.app/",
-              '_blank'
-            )
+            window.open("https://hushh-button.vercel.app/", "_blank")
           }
         >
-          <Icon as={HushhButton} />
-          <Text
-            ml={"20%"}
-            color={"#484848"}
-            fontWeight={"400"}
-            fontSize={{base:"1rem",md:"1.5rem"}}
-            lineHeight={{base:"14.56px",md:"30px"}}
+          <Stack
+            gap={"0.5rem"}
+            display={"flex"}
+            flexDirection={"row"}
+            padding={"2"}
+            my={2}
           >
-            Hushh Button
-          </Text>
-        </Button>
+            <Box
+              w={"100%"}
+              h={"100%"}
+              flex={0.2}
+              display={"flex"}
+              alignItems={"center"}
+              alignContent={"center"}
+              alignSelf={"center"}
+              justifyItems={"center"}
+              justifySelf={"center"}
+            >
+              <Image src={ButtonIcon} alt="hushhButtonIcon" objectFit="fill" />
+            </Box>
+            <VStack
+              gap={"0px"}
+              display={"flex"}
+              alignItems={"flex-start"}
+              textAlign={"left"}
+              flex={1}
+            >
+              <Text color={"#484848"} fontWeight={"400"} fontSize={"1rem"}>
+                Hushh Button
+              </Text>
+              <Text
+                fontWeight={"400"}
+                fontSize={"0.5rem"}
+                lineHeight={"10px"}
+                color={"#B1B1B1"}
+              >
+                Imagine a world where shopping feels effortless and tailored
+                just for you. Introducing the Hushh Button, a powerful plugin
+                that bridges the gap between your Hushh Wallet and your favorite
+                brands.
+              </Text>
+            </VStack>
+          </Stack>
+        </Box>
 
-        <Button
-          boxShadow={"4px 4px 0px 0px #BFBFBF"}
-          p={"0.1rem"}
+        <Box
+          bg={"white"}
           borderRadius={"8px"}
           border={"1px solid #000000"}
-          display={"flex"}
-          flexDirection={"row"}
-          justifyContent={"flex-start"}
-          bg={"white"}
-          color={"#484848"}
+          boxShadow={"4px 4px 0px 0px #BFBFBF"}
           onClick={() =>
             window.open(
               "https://chromewebstore.google.com/detail/hushh-browser-companion/glmkckchoggnebfiklpbiajpmjoagjgj?hl=en&authuser=0",
-              '_blank'
+              "_blank"
             )
           }
         >
-          <Icon as={ChromeExtension} />
-          <Text
-            ml={"20%"}
-            color={"#484848"}
-            fontWeight={"400"}
-            fontSize={{base:"1rem",md:"1.5rem"}}
-            lineHeight={{base:"14.56px",md:"30px"}}
+          <Stack
+            gap={"0.5rem"}
+            display={"flex"}
+            flexDirection={"row"}
+            padding={"2"}
+            my={2}
           >
-            Chrome Extension
-          </Text>
-        </Button>
+            <Box
+              w={"100%"}
+              h={"100%"}
+              flex={0.2}
+              display={"flex"}
+              alignItems={"center"}
+              alignContent={"center"}
+              alignSelf={"center"}
+              justifyItems={"center"}
+              justifySelf={"center"}
+            >
+              <Image src={ChromeIcon} alt="ChromeIcon" objectFit="fill" />
+            </Box>
+            <VStack
+              gap={"0px"}
+              display={"flex"}
+              alignItems={"flex-start"}
+              textAlign={"left"}
+              flex={1}
+            >
+              <Text color={"#484848"} fontWeight={"400"} fontSize={"1rem"}>
+                Hushh Chrome Extension
+              </Text>
+              <Text
+                fontWeight={"400"}
+                fontSize={"0.5rem"}
+                lineHeight={"10px"}
+                color={"#B1B1B1"}
+              >
+                Tired of being tracked online? Every click, scroll, and search
+                query leaves a trail of your identity, exploited by third-party
+                organizations for targeted advertising. With Hushh, the power is
+                back in your hands.
+              </Text>
+            </VStack>
+          </Stack>
+        </Box>
 
-        <Button
-          boxShadow={"4px 4px 0px 0px #BFBFBF"}
-          p={"0.1rem"}
+        <Box
+          bg={"white"}
           borderRadius={"8px"}
           border={"1px solid #000000"}
-          display={"flex"}
-          flexDirection={"row"}
-          justifyContent={"flex-start"}
-          bg={"white"}
-          color={"#484848"}
-          onClick={() =>
-            window.open(
-              "https://bit.ly/hushh-app-ios",
-              '_blank'
-            )
-          }
+          boxShadow={"4px 4px 0px 0px #BFBFBF"}
+          onClick={() => window.open("https://bit.ly/hushh-app-ios", "_blank")}
         >
-          <Icon as={HushhWalletApp} />
-          <Text
-            ml={"20%"}
-            color={"#484848"}
-            fontWeight={"400"}
-            fontSize={{base:"1rem",md:"1.5rem"}}
-            lineHeight={{base:"14.56px",md:"30px"}}
+          <Stack
+            gap={"0.5rem"}
+            display={"flex"}
+            flexDirection={"row"}
+            padding={"2"}
+            my={2}
           >
-            Hushh App
-          </Text>
-        </Button>
+            <Box
+              w={"100%"}
+              h={"100%"}
+              flex={0.2}
+              display={"flex"}
+              alignItems={"center"}
+              alignContent={"center"}
+              alignSelf={"center"}
+              justifyItems={"center"}
+              justifySelf={"center"}
+            >
+              <Image src={HushhAppIcon} alt="HushhAppIcon" objectFit="fill" />
+            </Box>
+            <VStack
+              gap={"0px"}
+              display={"flex"}
+              alignItems={"flex-start"}
+              textAlign={"left"}
+              flex={1}
+            >
+              <Text color={"#484848"} fontWeight={"400"} fontSize={"1rem"}>
+                Hushh App
+              </Text>
+              <Text
+                fontWeight={"400"}
+                fontSize={"0.5rem"}
+                lineHeight={"10px"}
+                color={"#B1B1B1"}
+              >
+                Imagine a world where your data works for you, not against you.
+                A world where your online and offline interactions paint a
+                detailed picture of your unique self, accessible only by you and
+                those you choose to trust.
+              </Text>
+            </VStack>
+          </Stack>
+        </Box>
 
-        <Button
+        <Box
+          bg={"white"}
+          borderRadius={"8px"}
+          border={"1px solid #000000"}
           boxShadow={"4px 4px 0px 0px #BFBFBF"}
           onClick={() =>
-            window.open(
-              "https://vibesearch-vercel.vercel.app",
-              '_blank'
-            )
+            window.open("https://vibesearch-vercel.vercel.app", "_blank")
           }
-          p={"0.1rem"}
-          display={"flex"}
-          flexDirection={"row"}
-          justifyContent={"flex-start"}
-          borderRadius={"8px"}
-          border={"1px solid #000000"}
-          bg={"white"}
-          color={"#484848"}
         >
-          <Icon as={VibeSearchApp} boxSize="xxs" />
-          <Text
-            ml={"20%"}
-            color={"#484848"}
-            fontWeight={"400"}
-            fontSize={{base:"1rem",md:"1.5rem"}}
-            lineHeight={{base:"14.56px",md:"30px"}}
+          <Stack
+            gap={"0.5rem"}
+            display={"flex"}
+            flexDirection={"row"}
+            padding={"2"}
+            my={2}
           >
-            Vibe Search
-          </Text>
-        </Button>
-        <Button
-          boxShadow={"4px 4px 0px 0px #BFBFBF"}
-          p={"0.1rem"}
-          borderRadius={"8px"}
-          display={"flex"}
-          flexDirection={"row"}
-          justifyContent={"flex-start"}
-          border={"1px solid #000000"}
+            <Box
+              w={"100%"}
+              h={"100%"}
+              flex={0.2}
+              display={"flex"}
+              alignItems={"center"}
+              alignContent={"center"}
+              alignSelf={"center"}
+              justifyItems={"center"}
+              justifySelf={"center"}
+            >
+              <Image
+                src={VibeSearchNew}
+                alt="VibeSearchNew"
+                objectFit="cover"
+              />
+            </Box>
+            <VStack
+              gap={"0px"}
+              display={"flex"}
+              alignItems={"flex-start"}
+              textAlign={"left"}
+              flex={1}
+            >
+              <Text color={"#484848"} fontWeight={"400"} fontSize={"1rem"}>
+                Hushh Vibe Search
+              </Text>
+              <Text
+                fontWeight={"400"}
+                fontSize={"0.5rem"}
+                lineHeight={"10px"}
+                color={"#B1B1B1"}
+              >
+                Discover the perfect product that matches your "vibe" with ease!
+                VIBE Search App is your personal shopping assistant that helps
+                you find what you're looking for, even if you don't know the
+                exact name or where to look for it!
+              </Text>
+            </VStack>
+          </Stack>
+        </Box>
+
+        <Box
           bg={"white"}
-          color={"#484848"}
+          borderRadius={"8px"}
+          border={"1px solid #000000"}
+          boxShadow={"4px 4px 0px 0px #BFBFBF"}
           onClick={() =>
             window.open(
               "https://www.hush1one.com/developer-Api/about-developer-api",
-              '_blank'
+              "_blank"
             )
           }
         >
-          <Icon as={VibeSearchAPIs} boxSize="xxs" />
-          <Text
-            ml={"20%"}
-            color={"#484848"}
+          <Stack
+            gap={"0.5rem"}
+            display={"flex"}
+            flexDirection={"row"}
+            padding={"2"}
+            my={2}
+          >
+            <Box
+              w={"100%"}
+              h={"100%"}
+              flex={0.2}
+              display={"flex"}
+              alignItems={"center"}
+              alignContent={"center"}
+              alignSelf={"center"}
+              justifyItems={"center"}
+              justifySelf={"center"}
+            >
+              <Image src={DeveloperIcon} alt="DeveloperIcon" objectFit="fill" />
+            </Box>
+            <VStack
+              gap={"0px"}
+              display={"flex"}
+              alignItems={"flex-start"}
+              textAlign={"left"}
+              flex={1}
+            >
+              <Text color={"#484848"} fontWeight={"400"} fontSize={"1rem"}>
+                Hushh Developer API
+              </Text>
+              <Text
+                fontWeight={"400"}
+                fontSize={"0.5rem"}
+                lineHeight={"10px"}
+                color={"#B1B1B1"}
+              >
+                Revolutionize your data game with Hushh Developer APIs! 
+                Tailored for businesses and organizations of all stripes, our
+                APIs empower YOUR developers to seamlessly integrate customers’
+                data into CRM systems and applications.
+              </Text>
+            </VStack>
+          </Stack>
+        </Box>
+        
+        <Box px={'15%'} h={'100%'} width={'100%'}>
+           <Button
+            as="a" 
+            href="/hushh-intro.pdf" 
+            download="HushhIntro"
+            borderRadius={"12px"}
+            bg={"#3045FF"}
+            h={'4rem'}
+            color={"#FFFFFF"}
+            letterSpacing={"-0.4px"}
+            fontSize={"1rem"}
             fontWeight={"400"}
-            fontSize={{base:"1rem",md:"1.5rem"}}
-            lineHeight={{base:"14.56px",md:"30px"}}
+            w="100%"
+            mt={10}
+            _hover={{
+              color: "white",
+              bg: "#1B1B1B",
+            }}
           >
-            Developer API
-          </Text>
-        </Button>
-
-        <a
-          href="/hushh-intro.pdf"
-          download="HushhInto"
-        >
-          <Button 
-          borderRadius={"12px"}
-          bg={"#3045FF"}
-          color={"#FFFFFF"}
-          letterSpacing={"-0.4px"}
-          fontSize={"1rem"}
-          fontWeight={"400"}
-          w="full"
-          mt={10}
-          _hover={{
-            color:'white',
-            bg:'#1B1B1B'
-          }}
-          >
-            Download Intro
+           <Text my={'2rem'}>Download Intro</Text> 
           </Button>
-        </a>
+        </Box>
+          
+
       </VStack>
     </Box>
   );

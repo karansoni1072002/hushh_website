320c88d05a907d16595ad68301e147287b8a801a
Neelesh <damrianeelesh@gmail.com>
2024-05-23 12:57:35 +0530

    udpdating blogs site metadata

--- src/app/sitemetadata.jsx
+++ src/app/sitemetadata.jsx
@@ -1,21 +1,21 @@
 // please update required information
 const siteMetadata = {
-  title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-  author: 'CodeBucks',
-  headerTitle: 'Next.js Blog',
-  description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+  title: 'Hushh Blogs',
+  author: 'Manish Sainani',
+  headerTitle: 'Hushh Blogs',
+  description: 'Your Data Your Business - with Hushh',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+  siteUrl: 'https://www.hushh1one.com/hushhBlogs', // your website URL
   siteLogo: '/logo.png',
   socialBanner: '/social-banner.png', // add social banner in the public folder
-  email: 'codebucks27@gmail.com', 
-  github: 'https://github.com/codebucks27',
-  twitter: 'https://twitter.com/code_bucks',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com/codebucks',
-  linkedin: 'https://www.linkedin.com/in/codebucks/',
-  dribbble: 'https://www.dribbble.com',
+  email: 'info@hushh1one.com', 
+  github: '',
+  twitter: 'https://x.com/hushh_ai',
+  facebook: '',
+  youtube: 'https://www.youtube.com/@hushhai',
+  linkedin: 'https://www.linkedin.com/company/hushh-ai/',
+  dribbble: 'https://www.hushh1one.com',
   locale: 'en-US',
 }
 

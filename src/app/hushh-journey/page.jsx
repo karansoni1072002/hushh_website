af45d21487d1c508534380f91ab79e6421c09c87
Neelesh <damrianeelesh@gmail.com>
2024-07-02 16:10:34 +0530

    flipbook added

---
+++ src/app/hushh-journey/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Flipbook from '../_components/flipbook'
+
+const hushhJourney = () => {
+  return (
+ <> 
+   <Flipbook/>
+ </>
+)
+}
+
+export default hushhJourney

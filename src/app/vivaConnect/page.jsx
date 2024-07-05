70f0cd83faa2a8d29432eea52ebb756199f7ab2d
Neelesh <damrianeelesh@gmail.com>
2024-07-05 11:29:33 +0530

    links updations

--- src/app/vivaConnect/page.jsx
+++ src/app/vivaConnect/page.jsx
@@ -515,11 +515,10 @@ export default function vivaConnect() {
                   }}
                   bg={"black"}
                   onClick={() =>
-                    // window.open("https://bit.ly/hushh-app-android", "_blank")
                     window.open("https://drive.google.com/file/d/1yuinnVcBv2bICFyaacAJrft2W6iEp1MV/view","_blank")
                   }
                   w={{ base: "100%", md: "auto" }}
-             
+                >
                   Android
                 </Button>
               </Box>

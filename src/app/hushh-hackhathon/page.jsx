f4d59b27a92130da9bd16696a6782ed8a82fc93a
Neelesh <damrianeelesh@gmail.com>
2024-07-08 19:43:21 +0530

    SEO additions

--- src/app/hushh-hackhathon/page.jsx
+++ src/app/hushh-hackhathon/page.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import HushhHackhathon from '../clientside/HushhHackhathon'
 
+export const metadata = {
+  title: "Hushh | Data Nexus Hackhathon",
+  description:
+    "hackathon data nexus project on hushh.ai ",
+  keywords:
+    "HUSHH, Hackhathon Submission, Data Nexus, data nexus, hushh hackhathon, hushh.ai, project upload, project submission, assignment submission, hackhathon submission, data nexus, Hushh Data Nexus, Extended Reality, IOT, Internet of things, Blockchain, 3D Modelling, In terms of technologies, Hushh Datanexus  will leverage the emerging technologies and technologies of tomorrow, specifically in the realm of Metaverse and AI. These themes and technologies provide a broad scope for participants to explore and develop innovative solutions that address real-world challenges in various domains",
+};
+
 const page = () => {
   return (
    <HushhHackhathon/>

0d6a0b89c69dee1bc2cfb6798c5fa53fae52f786
Neelesh <damrianeelesh@gmail.com>
2024-07-28 13:47:29 +0530

    updating community page

--- src/app/clientside/HushhCommunity.jsx
+++ src/app/clientside/HushhCommunity.jsx
@@ -93,162 +93,17 @@ const HushhCommunity = () => {
                 </Stack>
                 </Box> 
                 <Box w={'100%'} flex={0.7} position={'relative'}>
-                <Image src={hushhLogo} style={{zIndex:'4'}} title="" alt=""/>
+                <Image src={hushhLogo} style={{zIndex:'4'}} title="Hushh Logo" alt="Hushh Logo"/>
                 </Box>
                 <Image src={Ellipse} alt="Ellipse"  style={{position:'absolute',minWidth:'100%',opacity:'92%' ,zIndex:'3'}}  title="Ellipse"/>
             </HStack>
         </VStack>
 
-        <VStack
-          gap={{ md: "2rem", base: "1rem" }}
-          my={{ md: "6rem", base: "3rem" }}
-        >
-          <Text
-            className="gradient"
-            fontWeight={"700"}
-            fontSize={{ md: "4.75rem", base: "2.15rem" }}
-            lineHeight={{ md: "91px", base: "40px" }}
-          >
-            Our Communities
-          </Text>
-          <Text
-            px={{ md: "8rem", base: "2rem" }}
-            display={"flex"}
-            textAlign={"center"}
-            color={"#ABABAB"}
-            fontWeight={"500"}
-            fontSize={{ md: "1rem", base: "0.5rem" }}
-            lineHeight={{ md: "32px", base: "20px" }}
-          >
-            Visioncraft is an employee-centred company that looks after every
-            employee, gives autonomy to make choices, supports self-development
-            and career growth. Our development team is always in search of
-            talented individuals to join our employee-centred culture.
-          </Text>
-          <Stack
-            mt={{ md: "4rem", base: "2rem" }}
-            px={{ md: "8rem", base: "2rem" }}
-            display={"flex"}
-            flexDirection={"column"}
-            gap={{ md: "3rem", base: "1.5rem" }}
-          >
-            <HStack gap={{ md: "4rem", base: "1.5rem" }}>
-              <Image
-                src={discordIcon}
-                alt="Hushh Discord Channel"
-                title="join our hushh discord community"
-              />
-              <Text
-                color={"#ABABAB"}
-                fontWeight={"500"}
-                fontSize={{ md: "1rem", base: "0.65rem" }}
-              >
-                Visioncraft is an employee-centred company that looks after
-                every employee, gives autonomy to make choices, supports
-                self-development and career growth. Our development team is
-                always in search of talented individuals to join our
-                employee-centred culture.
-              </Text>
-              <Button
-                _hover={{ color: "black", bg: "white" }}
-                w={{ md: "10rem", base: "5rem" }}
-                color={"#FFFFFF"}
-                background={
-                  "linear-gradient(265.3deg, #E54D60 8.81%, #A342FF 94.26%)"
-                }
-                fontWeight={"600"}
-                fontSize={{ md: "11px", base: "7px" }}
-                lineHeight={"12px"}
-              >
-                Join
-              </Button>
-            </HStack>
-            <HStack gap={{ md: "4rem", base: "1.5rem" }}>
-              <Image
-                src={discordIcon}
-                alt="Hushh Discord Channel"
-                title="join our hushh discord community"
-              />
-              <Text
-                color={"#ABABAB"}
-                fontWeight={"500"}
-                fontSize={{ md: "1rem", base: "0.65rem" }}
-              >
-                Visioncraft is an employee-centred company that looks after
-                every employee, gives autonomy to make choices, supports
-                self-development and career growth. Our development team is
-                always in search of talented individuals to join our
-                employee-centred culture.
-              </Text>
-              <Button
-                _hover={{ color: "black", bg: "white" }}
-                w={{ md: "10rem", base: "5rem" }}
-                color={"#FFFFFF"}
-                background={
-                  "linear-gradient(265.3deg, #E54D60 8.81%, #A342FF 94.26%)"
-                }
-                fontWeight={"600"}
-                fontSize={{ md: "11px", base: "7px" }}
-                lineHeight={"12px"}
-              >
-                Join
-              </Button>
-            </HStack>
-            <HStack gap={{ md: "4rem", base: "1.5rem" }}>
-              <Image
-                src={discordIcon}
-                alt="Hushh Discord Channel"
-                title="join our hushh discord community"
-              />
-              <Text
-                color={"#ABABAB"}
-                fontWeight={"500"}
-                fontSize={{ md: "1rem", base: "0.65rem" }}
-              >
-                Visioncraft is an employee-centred company that looks after
-                every employee, gives autonomy to make choices, supports
-                self-development and career growth. Our development team is
-                always in search of talented individuals to join our
-                employee-centred culture.
-              </Text>
-              <Button
-                _hover={{ color: "black", bg: "white" }}
-                w={{ md: "10rem", base: "5rem" }}
-                color={"#FFFFFF"}
-                background={
-                  "linear-gradient(265.3deg, #E54D60 8.81%, #A342FF 94.26%)"
-                }
-                fontWeight={"600"}
-                fontSize={{ md: "11px", base: "7px" }}
-                lineHeight={"12px"}
-              >
-                Join
-              </Button>
-            </HStack>
-          </Stack>
+        <VStack display={'flex'} textAlign={'center'} alignItems={'center'}>
+            <Text className="gradient" fontWeight={'400'} fontSize={{md:'3.75rem',base:'1.9rem'}} lineHeight={{md:'75px',base:'40px'}} as={'h1'} >Join the <br/> Hushh Movement</Text>
+            <Text mx={{md:'9rem',base:'1.25rem'}} color={'#FFFFFF'} lineHeight={{md:'31.5px',base:'28px'}} fontSize={{md:'1.15rem',base:'0.75rem'}} fontWeight={'400'}>Whether you're a data-savvy customer, an empowered sales agent, or an innovative developer, Hushh has something for you. Let's work together to build a future where data empowers individuals and businesses alike.</Text>
         </VStack>
-        <VStack>
-          <Text
-            className="gradient"
-            fontSize={{ md: "4.75rem", base: "2.15rem" }}
-            fontWeight={"700"}
-            lineHeight={{ md: "91px", base: "40px" }}
-          >
-            Benefits of our community
-          </Text>
-          <Text
-            mx={{ md: "25%", basse: "1.25rem" }}
-            display={"flex"}
-            textAlign={"center"}
-            color={"#E4E4E4"}
-          >
-            In terms of technologies, Hushh Datanexus will leverage the emerging
-            technologies and technologies of tomorrow, specifically in the realm
-            of Metaverse and AI. These themes and technologies provide a broad
-            scope for participants to explore and develop innovative solutions
-            that address real-world challenges in various domains.
-          </Text>
-        </VStack>
+   
         <ContactForm />
       </Box>
     </>

43152c8b8ab551f6d033a9708e070d80acf95dff
Neelesh <damrianeelesh@gmail.com>
2024-08-04 18:08:03 +0530

    adding more changes for grid images layout

--- src/app/_components/features/dynamicImageGrid.jsx
+++ src/app/_components/features/dynamicImageGrid.jsx
@@ -1,19 +1,32 @@
-'use client'
-import { useState, useEffect } from 'react';
-import { Box, Grid } from '@chakra-ui/react';
-import square1 from '../svg/ImageGrid/square1.svg';
-import square2 from '../svg/ImageGrid/sqaure2.svg';
-import Vertical1 from '../svg/ImageGrid/Vertical1.svg';
-import Image from 'next/image';
+"use client";
+import { useState, useEffect } from "react";
+import { Box, Grid } from "@chakra-ui/react";
+import Image from "next/image";
+import Horizontal1 from "../../../../public/Images/gridImages/Horizontal 1.jpeg";
+import Horizontal2 from "../../../../public/Images/gridImages/Horizontal 2.jpeg";
+import Horizontal3 from "../../../../public/Images/gridImages/Horizontal 3.jpeg";
+import Horizontal4 from "../../../../public/Images/gridImages/Horizontal 4.jpg";
+import Vertical1 from "../../../../public/Images/gridImages/Vertical 1.jpeg";
+import Vertical2 from "../../../../public/Images/gridImages/Vertical 2.jpeg";
+import Vertical3 from "../../../../public/Images/gridImages/Vertical 3.jpeg";
+import Vertical4 from "../../../../public/Images/gridImages/Vertical 4.jpeg";
+import Vertical5 from "../../../../public/Images/gridImages/Vertical 5.jpg";
+import Vertical6 from "../../../../public/Images/gridImages/Vertical 6.jpg";
+import Vertical7 from "../../../../public/Images/gridImages/Vertical 7.jpg";
 
 const images = {
   left: [
     Vertical1,
-    Vertical1,
+    Vertical2,
+    Vertical3,
+    Vertical4,
+    Vertical5,
+    Vertical6,
+    Vertical7,
   ],
   right: [
-    [square1, square2],
-    [square2, square1],
+    [Horizontal1, Horizontal2, Horizontal3, Horizontal4],
+    [Horizontal4, Horizontal3, Horizontal2, Horizontal1],
   ],
 };
 
@@ -32,13 +45,21 @@ export default function ImageGrid() {
 
   return (
     <Box>
-      <Grid templateColumns="1fr 1fr">
-        <Grid templateRows={'2fr'}>
+      <Grid display={'flex'} templateColumns="1fr 1fr">
+        <Grid flex={1} templateRows={"2fr"}>
           <Image src={images.left[currentLeftImage]} alt="Left Image" />
         </Grid>
-        <Grid templateRows="1fr 1fr">
-          <Image src={images.right[currentRightImages][0]} alt="Right Top Image" />
-          <Image src={images.right[currentRightImages][1]} alt="Right Bottom Image" />
+        <Grid flex={1} flexDirection={'column'} gap={'0'}  h={'100%'}>
+          <Image
+            src={images.right[currentRightImages][0]}
+            alt="Right Top Image"
+            style={{height:'100%', width:'100%'}}
+          />
+          <Image
+            src={images.right[currentRightImages][1]}
+            alt="Right Bottom Image"
+            style={{height:'77.75%',width:'100%'}}
+          />
         </Grid>
       </Grid>
     </Box>

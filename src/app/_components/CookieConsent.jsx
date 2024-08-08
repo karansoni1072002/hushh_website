c4862c72cf8d0efcba48f2da1a49828f52670a7c
Neelesh <damrianeelesh@gmail.com>
2024-08-08 13:43:54 +0530

    local consent

--- src/app/_components/CookieConsent.jsx
+++ src/app/_components/CookieConsent.jsx
@@ -21,13 +21,13 @@ const CookieConsent = () => {
   }
 
   return (
-    <div className="fixed inset-0 bg-slate-700 bg-opacity-70 z-[5000]">
-      <div className='fixed bottom-0 left-0 flex items-center justify-between px-4 py-8 bg-orange-400 text-#fff '>
+    <div className="fixed w-full inset-0 bg-slate-700 bg-opacity-70 z-[5000]">
+      <div className='fixed bottom-0 left-0 right-0 flex items-center justify-between px-4 py-4 md:px-2 bg-orange-400 text-#fff '>
         <span className='text-dark text-base mr-16 '>
            This website use cookies to imporve use experience. By using our website you consent to all the Terms of Usage in accordance with our <Link href="/privacy-policy" className='font-bold text-custom'>Privacy Policy</Link>.
         </span>
 
-        <button className='bg-gray py-2 px-10 rounded' onClick={() => acceptCookie()}>
+        <button className='bg-white py-2 px-10 rounded' onClick={() => acceptCookie()}>
         Accept
       </button>
       </div>

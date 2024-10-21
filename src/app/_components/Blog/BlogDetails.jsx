c48eefb17239d89505828f942ab95c6375624289
DamriaNeelesh <damrianeelesh@gmail.com>
2024-10-21 22:27:46 +0530

    blog details page mobile UI fixes

--- src/app/_components/Blog/BlogDetails.jsx
+++ src/app/_components/Blog/BlogDetails.jsx
@@ -7,7 +7,7 @@ import ViewCounter from "./ViewCounter";
 
 const BlogDetails = ({ blog, slug: blogSlug }) => {
   return (
-    <Box py="4" mb={"4"} display="flex" alignItems="flex-start" justifyContent="flext-start" textAlign={'left'} flexWrap="wrap" rounded="lg">
+    <Box py="4" mb={"4"} display={{md:"flex",base:'none'}} alignItems="flex-start" justifyContent="flext-start" textAlign={'left'} flexWrap="wrap" rounded="lg">
       <Grid templateColumns={{ base: "1fr", md: "1fr 1fr" }} display={{md:'block',base:'none'}} gap={4}>
         <GridItem>
           <Box m="3" textAlign="left" textColor="white" fontWeight="400">

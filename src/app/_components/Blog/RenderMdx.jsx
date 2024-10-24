bb2f1cd4dfc6d60996c5cf11ca8917d648ab71fe
DamriaNeelesh <damrianeelesh@gmail.com>
2024-10-24 17:26:12 +0530

    updating text and redirect link on new tab for blog links

--- src/app/_components/Blog/RenderMdx.jsx
+++ src/app/_components/Blog/RenderMdx.jsx
@@ -9,6 +9,10 @@ import HushhWalletIcon from '../svg/hushhWalletIcon'
 import dynamic from 'next/dynamic';
 const Mermaid = dynamic(() => import('../hooks/useMermaid'), { ssr: false });
 
+const CustomLink = (props) => {
+  return <a {...props} target="_blank" rel="noopener noreferrer" />;
+};
+
 const mdxComponents = {
     Image,
     SyntaxHighlighter,
@@ -16,6 +20,7 @@ const mdxComponents = {
     ServiceCard,
     HushhWalletIcon,
     Mermaid,
+    a: CustomLink, // Override the default link component
 }
 
 const RenderMdx = ({blog}) => {
@@ -24,21 +29,26 @@ const RenderMdx = ({blog}) => {
   return (
     <>
     <div className='col-span-12 text-white lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
-    prose-blockquote:bg-accent/20 
-    prose-blockquote:p-2
-    prose-blockquote:px-6
-    prose-blockquote:border-accent
-    prose-blockquote:not-italic
-    prose-blockquote:rounded-r-lg
-    prose-li:marker:text-white
-    dark:prose-invert
-    dark:prose-blockquote:border-accentDark
-    dark:prose-blockquote:bg-accentDark/20
-    dark:prose-li:marker:text-accentDark
-    first-letter:text-3xl
-    sm:first-letter:text-5xl
-    prose-headings:text-white
-    '> 
+prose-blockquote:bg-accent/20 
+prose-blockquote:p-2
+prose-blockquote:px-6
+prose-blockquote:border-accent
+prose-blockquote:not-italic
+prose-blockquote:rounded-r-lg
+prose-li:marker:text-white
+prose-strong:text-white
+prose-b:text-white
+prose-p:text-white
+prose-span:text-white
+prose-a:text-white
+dark:prose-invert
+dark:prose-blockquote:border-accentDark
+dark:prose-blockquote:bg-accentDark/20
+dark:prose-li:marker:text-accentDark
+first-letter:text-3xl
+sm:first-letter:text-5xl
+prose-headings:text-white
+'>
         <MDXContent components={mdxComponents}/>
     </div>
     </>

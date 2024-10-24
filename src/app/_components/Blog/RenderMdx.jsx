70341fdcdddd7f3f6a7953cd72c9019e5b0c4d78
DamriaNeelesh <damrianeelesh@gmail.com>
2024-10-24 15:03:23 +0530

    heading color fix

--- src/app/_components/Blog/RenderMdx.jsx
+++ src/app/_components/Blog/RenderMdx.jsx
@@ -6,10 +6,8 @@ import SyntaxHighlighter from 'react-syntax-highlighter'
 import { Button } from '@chakra-ui/react'
 import { ServiceCard } from '../primitives/serviceCard'
 import HushhWalletIcon from '../svg/hushhWalletIcon'
-// import useMermaid from '../hooks/useMermaid'
 import dynamic from 'next/dynamic';
 const Mermaid = dynamic(() => import('../hooks/useMermaid'), { ssr: false });
-
 
 const mdxComponents = {
     Image,
@@ -25,28 +23,25 @@ const RenderMdx = ({blog}) => {
 
   return (
     <>
-    
-    <div className='col-span-12 text-white  lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
+    <div className='col-span-12 text-white lg:col-span-8 font-in prose sm:prose-base md:prose-lg max-w-max
     prose-blockquote:bg-accent/20 
     prose-blockquote:p-2
     prose-blockquote:px-6
     prose-blockquote:border-accent
     prose-blockquote:not-italic
     prose-blockquote:rounded-r-lg
-
     prose-li:marker:text-white
-
-    
     dark:prose-invert
     dark:prose-blockquote:border-accentDark
     dark:prose-blockquote:bg-accentDark/20
     dark:prose-li:marker:text-accentDark
     first-letter:text-3xl
     sm:first-letter:text-5xl
+    prose-headings:text-white
     '> 
         <MDXContent components={mdxComponents}/>
     </div>
-</>
+    </>
   )
 }
 
